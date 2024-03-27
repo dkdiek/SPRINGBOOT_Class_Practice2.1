e847fc5029276836fb8d8cbb19180c5b1d332889
k <developerlyk@gmail.com>
2024-03-27 18:24:36 +0900

    JWT 추가

---
+++ src/main/java/dev/ohhoonim/demo/service/AuthService.java
@@ -0,0 +1,25 @@
+package dev.ohhoonim.demo.service;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+import dev.ohhoonim.demo.config.service.JwtService;
+import dev.ohhoonim.demo.controller.dto.AuthResponse;
+import dev.ohhoonim.demo.model.User;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    
+    // public User authenticate(User user) {
+    public AuthResponse authenticate(User user) {
+       authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword())
+       ) ;
+       return new AuthResponse(jwtService.generateToken(user));
+    }
+    
+}
